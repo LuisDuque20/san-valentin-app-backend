eb754dc842dfdad1b2aba4feeefeee0c31b66502
LuisDuque20 <00013423@uca.edu.sv>
2026-02-12 19:28:02 -0600

    cors

--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ const app = require('./app');
 
 const PORT = 3000;
 
+app.use(cors({
+  origin: "*"
+}));
+
+
 app.listen(PORT, () => {
   console.log(`💌 Servidor corriendo en http://localhost:${PORT}`);
 });
